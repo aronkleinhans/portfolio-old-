4820b6f61d432167b52ce981c25835a7cbeac65d
aronKleinhans <kleinhans.aron@gmail.com>
2021-03-17 01:49:43 +0100

    dark theme polish

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,7 @@
 // darkmode
 let checkbox = document.getElementById('darkmodeBox');
 let label = document.getElementById('darkmode');
+let items = document.getElementsByClassName('item');
 
 if(localStorage.getItem('dark')) {
      document.body.classList.add('dark');
@@ -15,9 +16,15 @@ function switchDarkmode(){
           if(this.checked) {
                document.body.classList.add('dark');
                label.classList.add('checked');
+               for (let i = 0; i < items.length; i++) {
+                    items[i].classList.add('darkitem');
+               }
           } else {
                document.body.classList.remove('dark');
                label.classList.remove('checked');
+               for (let i = 0; i < items.length; i++) {
+                    items[i].classList.remove('darkitem');
+               }
                localStorage.removeItem('dark');
           };
      });
