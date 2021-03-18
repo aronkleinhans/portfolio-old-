ab1a6b9c834248c55e9dc9481d511571491af8ed
aronKleinhans <kleinhans.aron@gmail.com>
2021-03-18 12:48:22 +0100

    add switching button text to darkmode

--- resources/main.js
+++ resources/main.js
@@ -2,10 +2,12 @@
 let checkbox = document.getElementById('darkmodeBox');
 let label = document.getElementById('darkmode');
 let items = document.getElementsByClassName('item');
+let buttonText = document.getElementById('darkmode');
 
 if(localStorage.getItem('dark')) {
      document.body.classList.add('dark');
      label.classList.add('checked');
+     buttonText.innerText = 'Light Theme';
 };
 
 window.onload = switchDarkmode();
@@ -16,10 +18,12 @@ function switchDarkmode(){
           if(this.checked) {
                document.body.classList.add('dark');
                label.classList.add('checked');
+               buttonText.innerHTML = 'Light Theme';
           } else {
                document.body.classList.remove('dark');
                label.classList.remove('checked');
                localStorage.removeItem('dark');
+               buttonText.innerHTML = 'Dark Theme';
           };
      });
 };
