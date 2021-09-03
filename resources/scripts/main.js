c821f3f1194b3f0b96ab0e27841c26d4b40092fa
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-03 23:17:12 +0200

    try stuff

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,6 +3,9 @@ import ProjectApi from "./getRepos.js";
 const data = ProjectApi.getProjects;
 let tag = document.createElement("p");
 
+console.log(data);
+console.log(data());
+
 tag.appendChild(data);
 
 let element = document.getElementsByTagName("body");
