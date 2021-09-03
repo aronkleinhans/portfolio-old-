07aa2d4f717b03e8293e2c544e831c7711cd1f14
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-04 00:50:42 +0200

    try stuff

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,7 +3,6 @@ import ProjectApi from "./getRepos.js";
 const data = ProjectApi.getProjects;
 let element = document.getElementById("test");
 let tag = document.createElement("p");
-let projects = "";
 
 tag.innerText = `I'm your p tag and i got : ${data().projects[0]}`;
 element.appendChild(tag);
