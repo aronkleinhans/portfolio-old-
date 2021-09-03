76ca31eede75085fb17d0dbe264bc8ccc3d2aef5
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-04 00:39:06 +0200

    try stuff

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,12 +1,14 @@
 import ProjectApi from "./getRepos.js";
 
 const data = ProjectApi.getProjects;
-
 let tag = document.createElement("p");
+let projects = "";
 
 for(item in data()) {
-    tag.innerText += "/n" + item;
+    projects += "\n" + item;
 }
+
+tag.innerText = projects;
 
 let element = document.getElementById("test")
 
