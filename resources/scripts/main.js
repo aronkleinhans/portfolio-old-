997d18fa63d51ec6656ee94d89eb375795834ca4
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-03 10:28:19 +0200

    add code to fetched data on page

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,5 +1,10 @@
 import ProjectApi from "./getRepos.js";
 
 const data = ProjectApi.getProjects();
+let tag = document.createElement("p");
 
-console.log(data)
+tag.appendChild(data);
+
+let element = document.getElementsByTagName("body");
+
+element.appendChild(tag);
