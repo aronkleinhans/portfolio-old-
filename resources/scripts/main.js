ca27987974ce76e8ea97d52f4b8dc4d97fea668a
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-04 00:48:39 +0200

    try stuff

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,6 +3,7 @@ import ProjectApi from "./getRepos.js";
 const data = ProjectApi.getProjects;
 let element = document.getElementById("test");
 let tag = document.createElement("p");
+let tag2 = document.createElement("p");
 let projects = "";
 
 for(item in data()) {
@@ -13,3 +14,6 @@ console.log(projects);
 
 tag.innerText = projects;
 element.appendChild(tag);
+
+tag2.innerText = `I'm tag2 and i got : ${data().projects[0]}`;
+element.appendChild(tag2);
