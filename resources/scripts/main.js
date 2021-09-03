3e5f300b0576782feaf57d38c41fbdd85fa24d1d
aronkleinhans <74680502+aronkleinhans@users.noreply.github.com>
2021-09-03 11:59:03 +0200

    update main.js

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,7 +3,7 @@ import ProjectApi from "./getRepos.js";
 const data = ProjectApi.getProjects();
 let tag = document.createElement("p");
 
-tag.appendChild(data);
+tag.appendChild(data());
 
 let element = document.getElementsByTagName("body");
 
