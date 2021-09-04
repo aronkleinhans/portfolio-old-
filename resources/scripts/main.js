a54fc57f61afc104800b5d7e6ca27fe1ce707bc7
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-04 23:13:24 +0200

    test

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -4,5 +4,10 @@ const data = ProjectApi.getProjects;
 let element = document.getElementById("test");
 let tag = document.createElement("p");
 
+console.log(data())
+console.log(data().projects)
+console.log(data().projects[0])
+
+
 tag.innerText = `I'm your p tag and i got : ${data().projects[0]}`;
 element.appendChild(tag);
