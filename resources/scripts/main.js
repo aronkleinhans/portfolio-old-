87536d2d973f1ebdfdd644bc34c07811b4cb4e0d
aronkleinhans <kleinhans.aron@gmail.com>
2021-09-05 23:10:33 +0200

    finalize project fetcher & render scripts, add logo, add nav, update styles.css

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,12 +1,48 @@
 import ProjectApi from "./getRepos.js";
 
 const projects = document.getElementById('projects');
-let makeP = document.createElement("p");
-projects.appendChild(makeP)
+let divs = [];
 
 async function renderProjects() {
     const data = await ProjectApi.getProjects();
-    makeP.textContent = data.projects[0].description;
+    console.log(data)
+    for(let i = 0; i <= data.projects.length-1; i++) {
+    let addDiv = document.createElement("div");
+    let addImg = document.createElement("img");
+    let addP = document.createElement("p");
+    let addName = document.createElement("p");
+    let addGHLink = document.createElement("a");
+
+    if(i % 2 === 0) {
+        addDiv.style = "border-left: solid 5px rgb(170, 59, 133); border-bottom: solid 5px rgb(170, 59, 133);"
+    }
+    else {
+        addDiv.style = "border-right: solid 5px rgb(170, 59, 133); border-bottom: solid 5px rgb(170, 59, 133);"
+    }
+
+
+    addGHLink.appendChild(addName);
+    addDiv.appendChild(addImg);
+    addDiv.appendChild(addGHLink);
+    addDiv.appendChild(addP);
+    projects.appendChild(addDiv);
+
+    addName.textContent = data.projects[i].name;
+    addName.classList += "sText";
+    
+    addGHLink.href = data.projects[i].url;
+    addGHLink.target = "_blank";
+
+    addDiv.classList += "item";
+
+    addImg.classList += "itemImage";
+    addImg.src = data.projects[i].image;
+    addImg.alt = "screenshot of the code in action";
+
+    addP.textContent = data.projects[i].description;
+
+    divs.push(addDiv);
+    }
 }
 
 renderProjects();
